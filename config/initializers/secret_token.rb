4b7dc208ad6cb6a618f7ceda3f88960febb1dde3
Roger Graves <rogergraves@gmail.com>
2012-03-13 16:40:05 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomcheck::Application.config.secret_token = '3caef8dec0e9cf96bdd88285815c4b27ee28acee5fbf5f5ce158c956948e7bbea16df620d495f760ec3d8f3c2660596007da519ffebb885fcc86e564bf0f1208'
