19b2cd8b06f59712e6329ff7ee671ba7e7913c13
Kimberly Lanning <kimberly.lanning@gmail.com>
2012-03-16 13:16:00 -0700

    temporary commit before shereef starts messing around

--- spec/views/check_lists/index.html.erb_spec.rb
+++ spec/views/check_lists/index.html.erb_spec.rb
@@ -1,22 +1,26 @@
 require 'spec_helper'
-require 'factory_girl_rails'
 
-describe "check_lists/index.html.erb" do
+describe "check_lists/index.html.erb", :type => :request do
+  before :each do
+    @checklists = []
+    3.times do 
+      list = Factory(:check_list)
+      list.check_items = [Factory(:check_item)]
+      @checklists << list
+    end  
+    render
+  end
   
   context "viewing checklist items" do
-    it 'shows correct number of check items' do
-      @checklists = []
-      3.times do 
-        list = Factory(:check_list)
-        list.check_items = [Factory(:check_item)]
-        @checklists << list
-      end  
-      render
+    it "has correct number of check items" do
       rendered.should have_tag("li", :count => 3)
     end
   end
   
-  # it 'has an edit link' do
-  #     rendered.should have_link('edit', )
-  #   end
+  it 'has an edit link' do
+    click_link 'Edit'
+    response.should have_selector('form') do
+      with_tag "input", :with => { :name => "check_item[name]"}
+    end
+  end
 end
