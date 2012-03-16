53fa4790cb63acd99b3cc5a7844b48127f0c6519
Kimberly Lanning <kimberly.lanning@gmail.com>
2012-03-16 12:34:41 -0700

    Added viewing checklist items test"

--- spec/views/check_lists/index.html.erb_spec.rb
+++ spec/views/check_lists/index.html.erb_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 require 'factory_girl_rails'
 
 describe "check_lists/index.html.erb" do
-  before do
-    @list_item = Factory(:check_item)
-    @checklists = Factory(:check_list)
-    @list_item.check_list_id = @checklists.id
-    puts @list_item.check_list_id
-    puts @checklists.check_items.all
-    #assign :check_items, @list_item
-    assign :checklists, [@checklists]
-    render   
-  end
   
   context "viewing checklist items" do
     it 'shows correct number of check items' do
-      rendered.should have_tag("li", :count => 1)
+      @checklists = []
+      3.times do 
+        list = Factory(:check_list)
+        list.check_items = [Factory(:check_item)]
+        @checklists << list
+      end  
+      render
+      rendered.should have_tag("li", :count => 3)
     end
   end
+  
+  # it 'has an edit link' do
+  #     rendered.should have_link('edit', )
+  #   end
 end
