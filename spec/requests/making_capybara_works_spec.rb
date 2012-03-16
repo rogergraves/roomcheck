1e118fd7543b9586e3a9de6fc0bbd59adf8123cf
Kimberly Lanning <kimberly.lanning@gmail.com>
2012-03-16 15:44:46 -0700

    Passing RSpec integration test for checklist edit

--- spec/requests/making_capybara_works_spec.rb
+++ spec/requests/making_capybara_works_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe "MakingCapybaraWorks" do
+  before :each do
+    @checklists = []
+    3.times do 
+      list = Factory(:check_list)
+      list.check_items = [Factory(:check_item)]
+      @checklists << list
+    end
+  end
+  
   describe "GET /making_capybara_works" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get check_lists_path
-      response.status.should be(200)
+      visit check_lists_path
       click_link "Edit"
+      page.should have_content("CheckItems#edit")
     end
   end
 end
