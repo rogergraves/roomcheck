bd2ca01a4bf546dbcd38a40c2982314b426d1c32
Jason Bush <jasonbushis@gmail.com>
2012-03-19 11:39:18 -0700

    Adds check_items views add page

--- app/controllers/check_items_controller.rb
+++ app/controllers/check_items_controller.rb
@@ -1,11 +1,20 @@
 class CheckItemsController < ApplicationController
   def new
     @check_item = CheckItem.new
+    @check_item.check_list_id = params[:check_list_id]
+    #@check_list = CheckList
+    # @check_list = CheckList.find_by_id(params[:check_list_id])
   end
   
   def create
+    #@check_item = CheckItem.new(params[:check_item])
+    # list = CheckList.find_by_id(params[:check_list_id])
+    # check_item = list.check_items.build(params[:check_item])
+
+    
     @check_item = CheckItem.new(params[:check_item])
-    redirect_to(check_lists_path, :notice => "Item Saved") if @check_item.save
+    @check_item.save ? redirect_to(check_lists_path, :notice => "Item Saved") : flash[:error]
+
   end
 
   def edit
--- spec/views/check_items/new.html.erb_spec.rb
+++ spec/views/check_items/new.html.erb_spec.rb
@@ -12,10 +12,22 @@ describe "check_items/new" do
     end
     
     it "has a form for entering in a new item" do
-      rendered.should have_selector("form", :with => { :action => "new_check_item_path", :method => "post"}) do |form|
-        form.should
-        have_selector('label', :for => 'name', :content => 'name')
+      rendered.should have_tag("form", :with => { :action => "/check_items", :method => "post"}) do
+        with_tag "input", :with => { :name => "check_item[area]" }
       end
     end
   end
+  
+  context "errors" do
+      before do
+        check_item = CheckItem.new
+        check_item.valid?
+        assign(:check_item, check_item)
+        render
+      end
+      
+      it "displays errors when link saving fails" do
+        rendered.should have_content("can't be blank")
+      end
+    end
 end
