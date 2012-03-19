e333fa8a35b663a174e2dc7f2540e1564505a148
Ashley Wysocki <ashleygwysocki@gmail.com>
2012-03-19 11:02:10 -0700

    Added tests for editing result

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Roomcheck::Application.routes.draw do
   
   resources :results
-<<<<<<< HEAD
-=======
-  
-  # get "results/new"
-  # get "results/create"
-  # get "results/edit"
-  # get "results/update"
-  # get "results/destroy"
-  # get "results/show"
 
->>>>>>> c101e7ee5e8296906abdd4414ce8894e01487e5d
   resources :check_lists, :only => [ :index, :new, :edit, :destroy ]
   resources :units, :only => [ :show, :new, :destroy ]
   resources :check_items, :only => [ :new, :edit, :destroy ]
