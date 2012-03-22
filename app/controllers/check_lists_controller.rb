c830935e3ad31fdf8a96bc37d7263db87f96be04
Roger Graves <rogergraves@gmail.com>
2012-03-22 16:18:41 -0700

    adds new order to checklist items after drag and drop

--- app/controllers/check_lists_controller.rb
+++ app/controllers/check_lists_controller.rb
@@ -5,9 +5,7 @@ class CheckListsController < ApplicationController
   
   def show
     @checklist = CheckList.find(params[:id])
-    @checkitems = CheckItem.find_all_by_check_list_id(params[:id], :order => "item_order asc, id asc")
-    
-    logger.info "DEBUG:::::#{@checklists.inspect}"
+    @checkitems = CheckItem.find_all_by_check_list_id(params[:id])
   end
 
   def new
@@ -27,5 +25,31 @@ class CheckListsController < ApplicationController
     else
       flash[:error]
     end
+  end
+  
+  def update
+    order = params[:order]
+    new_order = order.split("X")
+    
+    order_hash = {}
+    i = 1
+    new_order.each do |check_item_id|
+      order_hash[check_item_id] = i
+      i += 1
+    end
+    
+    checkitems = CheckItem.find_all_by_check_list_id(CheckItem.find_by_id(order_hash.keys[0]).check_list_id)
+    checkitems.each do |check_item|
+      check_item.item_order = order_hash[check_item.id.to_s]
+      check_item.save
+    end
+    logger.info "DEBUG:::::#{checkitems.inspect}"
+    
+    logger.info "DEBUGGGGGG!!!!!: #{new_order}"
+    # @checkitem = CheckItem.find_by_order(params[:checklist][order])
+    # @checkitem.order = 
+    # respond_to do |format|
+    #    format.js 
+    #  end
   end
 end
