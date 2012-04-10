d10b695ff72ea686abd22920aa6443a0cfeee4b2
Roger Graves <rogergraves@gmail.com>
2012-04-10 13:00:44 -0700

    start on results controller spec

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -1,82 +1,38 @@
 require 'spec_helper'
 
 describe ResultsController do
-  
-  it '#create: adds a new result' do
-    result = Result.new :unit_id => 3, :check_item_id => 1
-    result.should be_valid
-  end
-
-  it '#create: doesnt add a new result without unit_id' do
-    result = Result.new :check_item_id => 1
-    result.should_not be_valid
-  end
-
-  it '#create: doesnt add a new result without check_item_id' do
-    result = Result.new :unit_id => 3
-    result.should_not be_valid
-  end
-  
-  it '#update: updates an existing result when passed parameters' do
-    result = Result.new :unit_id => 3, :check_item_id => 1
-    result.save
-    result.unit_id = 4
-    result.save
+  describe "GET index" do
     
-    result.unit_id.should == 4
   end
+  describe "POST create" do
+    
+  end
+  describe "GET new" do
+  
+  end
+  describe "GET edit" do
+    
+  end
+  describe "GET show" do
+    
+  end
+  describe "GET update" do
+    
+  end
+  describe "DELETE destroy" do
+    
+  end
+  
 
 
-  # create_table "results", :force => true do |t|
-  #   t.integer  "severity"
-  #   t.string   "comment"
-  #   t.integer  "unit_id"
-  #   t.integer  "check_item_id"
-  #   t.datetime "created_at",    :null => false
-  #   t.datetime "updated_at",    :null => false
-  # end
-
-
-  # describe "GET 'new'" do
-  #   it "returns http success" do
-  #     get 'new'
-  #     response.should be_success
-  #   end
-  # end
-  # 
-  # describe "GET 'create'" do
-  #   it "returns http success" do
-  #     get 'create'
-  #     response.should be_success
-  #   end
-  # end
-  # 
-  # describe "GET 'edit'" do
-  #   it "returns http success" do
-  #     get 'edit'
-  #     response.should be_success
-  #   end
-  # end
-  # 
-  # describe "GET 'update'" do
-  #   it "returns http success" do
-  #     get 'update'
-  #     response.should be_success
-  #   end
-  # end
-  # 
-  # describe "GET 'destroy'" do
-  #   it "returns http success" do
-  #     get 'destroy'
-  #     response.should be_success
-  #   end
-  # end
-  # 
-  # describe "GET 'show'" do
-  #   it "returns http success" do
-  #     get 'show'
-  #     response.should be_success
-  #   end
-  # end
 
 end
+
+# create_table "results", :force => true do |t|
+#   t.integer  "severity"
+#   t.string   "comment"
+#   t.integer  "unit_id"
+#   t.integer  "check_item_id"
+#   t.datetime "created_at",    :null => false
+#   t.datetime "updated_at",    :null => false
+# end
