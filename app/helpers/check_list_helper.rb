0c3cf62983fb7a60a0ecedecd6d1d91123551634
Roger Graves <rogergraves@gmail.com>
2012-04-09 23:52:47 -0700

    change color of edit problem button

--- app/helpers/check_list_helper.rb
+++ app/helpers/check_list_helper.rb
@@ -20,7 +20,7 @@ module CheckListHelper
     result = Result.find_by_check_item_id_and_completed_on(check_item_id, nil)
 
     if result && result.severity > 0
-      link_to("Edit Problem", edit_result_path(:id => result.id), :class => "btn btn-warning")
+      link_to("Edit Problem", edit_result_path(:id => result.id), :class => "btn btn-info")
     else
       link_to("Report Problem", new_result_path(:check_item_id => check_item_id), :class => "btn btn-warning")
     end   
