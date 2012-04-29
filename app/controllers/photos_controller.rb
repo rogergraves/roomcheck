8927d5d44013ed2dc174c27390648744f01f9c3b
Roger Graves <rogergraves@gmail.com>
2012-04-28 22:02:57 -0700

    add manual authentication token scheme for picup app uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,27 +3,30 @@ class PhotosController < ApplicationController
   
   def create
     result_id = params[:id]
+    user = User.find_by_encrypted_password(params[:authenticity_token])
+    
+    if(user)
+      if(result_id.to_i > 0)
+        result = Result.find(params[:id])
+      
+        result.image = params[:image]
+        result.severity = params[:severity] if params[:severity]
+        result.comment = params[:comment] if params[:comment]
+      
+        result.save
+      elsif(result_id == "new" && params[:check_item_id])
+        severity = 1
+        comment = ""
+      
+        severity = params[:severity] if params[:severity]
+        comment = params[:comment] if params[:comment]
+      
+        result = Result.new(check_item_id: params[:check_item_id], severity: severity, comment: comment, image: params[:image])
+        result_id = result.id if result.save
+      end
 
-    if(result_id.to_i > 0)
-      result = Result.find(params[:id])
-      
-      result.image = params[:image]
-      result.severity = params[:severity] if params[:severity]
-      result.comment = params[:comment] if params[:comment]
-      
-      result.save
-    elsif(result_id == "new" && params[:check_item_id])
-      severity = 1
-      comment = ""
-      
-      severity = params[:severity] if params[:severity]
-      comment = params[:comment] if params[:comment]
-      
-      result = Result.new(check_item_id: params[:check_item_id], severity: severity, comment: comment, image: params[:image])
-      result_id = result.id if result.save
+      render :text => result_id
     end
-
-    render :text => result_id
 
   end
   
