80007fa22e9955edb26bacb096a33b785fc8224c
Roger Graves <rogergraves@gmail.com>
2012-04-30 13:59:11 -0700

    add reorder capabilities to check item templates

--- app/controllers/check_item_templates_controller.rb
+++ app/controllers/check_item_templates_controller.rb
@@ -59,21 +59,25 @@ class CheckItemTemplatesController < ApplicationController
   # PUT /check_item_templates/1.json
   
   def reorder
-  
-    order = params[:order]
-    new_order = order.split("X")
-       
-       order_hash = {}
-       i = 1
-       new_order.each do |template_id|
-         order_hash[template_id] = i
-         i += 1
-       end
-       CheckItemTemplate.all.each do |template_item|
-         template_item.item_order = order_hash[template_item.id.to_s]
-         puts "********************* #{template_item.inspect}"
-         template_item.save
-       end  
+    new_order = params[:check_item_template].map {|e1, e2| [e1.to_i, e2.to_f]}.sort_by { |check_item_id, order| order }
+    
+    logger.info "\n\n!!!!!!!!!!!!!!!!!!\nCheckItemTemplatesController#reorder\n#{new_order.to_s}\n!!!!!!!!!!!!!!!!!!\n\n"
+    
+    order_hash = {}
+    i = 1
+    new_order.each do |check_item_template_id|
+      order_hash[check_item_template_id[0].to_s] = i
+      i += 1
+    end
+    
+    checkitemstemplates = CheckItemTemplate.all
+    checkitemstemplates.each do |check_item_template|
+      check_item_template.item_order = order_hash[check_item_template.id.to_s]
+      check_item_template.save
+    end
+    
+    flash[:notice] = "Check items reordered"
+    redirect_to check_item_templates_path
     
   end
   
